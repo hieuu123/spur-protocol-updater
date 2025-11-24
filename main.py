e0139967bd2807635938d06aa571f12b211f4dba
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-11-24 17:26:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
 POST_ID = 304392  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today for November 24, 2025"
-CHECK_ANSWER = "A) Manually or automatically, often via smart contracts."
+CHECK_ANSWER = "A) Manually or automatically, often via smart contracts.."
 
 # find & replace
 OLD_DATE = "November 24"
