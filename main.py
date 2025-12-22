ee7a6c8fed067e036ee8d158059eb08996d7c93d
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-12-22 10:24:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 309975  # ID bài Spur Protocol
+POST_ID = 310121  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "C) It help identify vulnerabilities before they cause financial loss."
+CHECK_ANSWER = "C) They coexist, each excelling in different areas of the ecosystem."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
