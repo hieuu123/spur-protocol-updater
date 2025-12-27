304d8c7c67b86ace25b5fae1b0ea6073a6d5e2ff
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-12-27 18:24:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 310953  # ID bài Spur Protocol
+POST_ID = 311110  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "D) It has a fixed supply with a maximum of 21 million coins."
+CHECK_ANSWER = "A) Because thousands of decentralized computers run the network continuously."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
