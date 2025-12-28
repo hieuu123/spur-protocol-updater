ea8a0b101d1b9d20e4e1bc8ae70c4e54ffe7486c
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-12-28 15:45:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 311110  # ID bài Spur Protocol
+POST_ID = 311192  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "A) Because thousands of decentralized computers run the network continuously."
+CHECK_ANSWER = "C) An attack where one person creates fake identities to gain unfair influence in a system."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
