b259a4b34ba79aac173f5aa074c5916bc341ce5a
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-12-30 14:18:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 311425  # ID bài Spur Protocol
+POST_ID = 311646  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "A) It separates functions into specialized layers to improve scalability."
+CHECK_ANSWER = "D) The cost shifts elsewhere, often affecting decentralization or users."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
