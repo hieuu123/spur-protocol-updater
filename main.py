fe3218bb1838ba62546a9da28444bae1bd2cdcdf
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-12-31 14:54:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 311646  # ID bài Spur Protocol
+POST_ID = 311863  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "D) The cost shifts elsewhere, often affecting decentralization or users."
+CHECK_ANSWER = "C) It achieves high speed by sacrificing some decentralization."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
