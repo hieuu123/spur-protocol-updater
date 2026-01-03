0464bf0f12753e4d1a522101753c128a90eea2c6
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-01-03 14:45:28 +0700

    Modify POST_ID and CHECK_ANSWER in main.py
    Updated POST_ID and CHECK_ANSWER values for the quiz.

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 312038  # ID bài Spur Protocol
+POST_ID = 312095  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "A) To verify user identities and prevent illegal activities like fraud."
+CHECK_ANSWER = "B) Creating tamper-proof digital diplomas and credentials."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
