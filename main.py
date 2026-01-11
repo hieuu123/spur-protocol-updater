d0a418339a763d7c9a3962366503731189daf69e
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-01-11 13:13:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 313870  # ID bài Spur Protocol
+POST_ID = 313926  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "C) A store of value often compared to digital gold."
+CHECK_ANSWER = "C) Hegota, through the introduction of Verkle Trees."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
