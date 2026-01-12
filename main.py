7c7eb7a3e224167513d084da299564489381b9f8
xialing267-droid <xialing267@gmail.com>
2026-01-12 15:39:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 313926  # ID bài Spur Protocol
+POST_ID = 314065  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "C) Hegota, through the introduction of Verkle Trees."
+CHECK_ANSWER = "A) The market is firmly in Bitcoin Season with BTC dominance high."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
