26f198f63e0bbcba5021fa0ca0cd3f1a1f6fc3b6
xialing267-droid <xialing267@gmail.com>
2026-01-22 09:29:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 316059  # ID bài Spur Protocol
+POST_ID = 316406  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "A) Assets can be bought or sold easily without large price impact."
+CHECK_ANSWER = "C) Off-chain storage integration with on-chain hashes."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
