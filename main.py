b59832da758483a3425e32f73e4b4657af3a1235
xialing267-droid <xialing267@gmail.com>
2026-01-27 08:32:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 317026  # ID bài Spur Protocol
+POST_ID = 317206  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "B) Sidechains are independent blockchains, while state channels enable off-chain transactions."
+CHECK_ANSWER = "C) To ensure trustless, all-or-nothing exchanges between different blockchains."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
