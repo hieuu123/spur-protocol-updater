1d18471fbde892a0b2ad2bae3cfefb4fc1461b01
xialing267-droid <xialing267@gmail.com>
2026-02-05 08:25:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 318599  # ID bài Spur Protocol
+POST_ID = 318898  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "D) Cash is required for wholesale interbank transactions."
+CHECK_ANSWER = "C) By making ledger manipulation economically impractical."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
