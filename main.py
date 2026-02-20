a446781ab8e1f0d8710556e602c9558347a74907
xialing267-droid <xialing267@gmail.com>
2026-02-20 09:11:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 320416  # ID bài Spur Protocol
+POST_ID = 320450  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "....."
+CHECK_ANSWER = "C) Always double-check the address before confirming the transaction."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
