c9730812b85d77ba9fde159bf525a3a4ed2ecdc9
xialing267-droid <xialing267@gmail.com>
2026-02-26 07:24:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 320971  # ID bài Spur Protocol
+POST_ID = 321148  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "D) To improve security by enabling checksum error detection."
+CHECK_ANSWER = "B) Because wallet addresses are generated using complex cryptographic algorithms and extremely large number spaces."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
