bc96f6d342a09ac109ef3a01f6e4e40e5cee45af
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-03-15 16:20:08 +0700

    Update POST_ID and CHECK_ANSWER values

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 324068  # ID bài Spur Protocol
+POST_ID = 324115  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "C) They create fixed-length outputs that reveal data tampering."
+CHECK_ANSWER = "B) Ensure nodes agree on the correct ledger state."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
