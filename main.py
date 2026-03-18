33a58a0868b7375ae21ce985c3a4d25829784465
xialing267-droid <xialing267@gmail.com>
2026-03-18 08:21:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 324486  # ID bài Spur Protocol
+POST_ID = 324671  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
 CHECK_ANSWER = "..."
 
