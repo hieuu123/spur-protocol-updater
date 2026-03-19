7ccaeb76cefe4ea8024c1f04d4c7adf8c0fbcc94
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-03-19 14:22:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 324671  # ID bài Spur Protocol
+POST_ID = 324917  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "..."
+CHECK_ANSWER = "PoW uses computational power, while PoS relies on staked assets."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
