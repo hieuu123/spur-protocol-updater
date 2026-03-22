9a6e881c2d7925a244f2536fec523521ab8b266b
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-03-22 16:50:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 325296  # ID bài Spur Protocol
+POST_ID = 325395  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "A) Network demand and computational complexity."
+CHECK_ANSWER = "D) Consensus combined with transaction ordering."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
