5cb2debe92ab7aba5a4d37c4066747aabc4234d1
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-04-10 15:37:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 329871  # ID bài Spur Protocol
+POST_ID = 330080  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "C) Reusing a valid transaction on another network."
+CHECK_ANSWER = "A) Block can’t be reversed once confirmed."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
