78db60b6565b29e25e39b2ad4b9dd4bf82669195
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2026-04-15 16:05:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 330546  # ID bài Spur Protocol
+POST_ID = 330891  # ID bài Spur Protocol
 TARGET_H2_TEXT = "Spur Protocol Quiz Answers Today - December 19, 2025"
-CHECK_ANSWER = "C) Deployed code is difficult to change after launch."
+CHECK_ANSWER = "B) Borrowing large funds and repaying within one transaction."
 
 # ================ SCRAPE SITE ================
 def scrape_quiz_site():
